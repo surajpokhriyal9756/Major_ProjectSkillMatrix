9c6a059b95b91e63ec19a14a84bfdc3d1b1dcf64
surajpokhriyal9756 <92304712+surajpokhriyal9756@users.noreply.github.com>
2024-05-14 17:21:10 +0530

    Update UserManagement.js

--- frontend_mp/src/Components/UserManagement.js
+++ frontend_mp/src/Components/UserManagement.js
@@ -11,6 +11,8 @@ const UserDetails = ({ setCheck, setCheckApprover, setCheckUser }) => {
   const [userDetail, setUserDetail] = useState(null);
   const [error, setError] = useState("");
   const [empId, setEmpId] = useState("");
+  const [skills, setSkills] = useState([]);
+  const [certificates, setCertificates] = useState([]);
 
   const userData = location.state.userData;
 
@@ -49,13 +51,49 @@ const UserDetails = ({ setCheck, setCheckApprover, setCheckUser }) => {
 
     navigate("/data", { state: { userData: userData } });
   };
+
+  const fetch_skills = async (emp_id) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/userRouterSkills/getSkills",
+        { emp_id }
+      );
+      if (!response.data || !response.data.length) {
+        throw new Error("Skills not found");
+      }
+      setSkills(response.data);
+    } catch (error) {
+      console.log("here");
+      console.error(error);
+    }
+  };
+
+  const fetchCertificates = async (emp_id) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/userRouterSkills/getCertificates",
+        { emp_id }
+      );
+      if (!response.data || !response.data.length) {
+        throw new Error("Certificates not found");
+      }
+      setCertificates(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const handleSearch = async () => {
     try {
+      setSkills([]);
+      setCertificates([]);
       const response = await axios.post(
         "http://localhost:4000/userRouterSkills/getUserDetails",
         { empId }
       );
       setUserDetail(response.data); // Set the retrieved user data in state
+      fetch_skills(empId);
+      fetchCertificates(empId);
       setError(""); // Clear any previous errors
     } catch (error) {
       console.error("Error fetching user details:", error);
@@ -64,12 +102,13 @@ const UserDetails = ({ setCheck, setCheckApprover, setCheckUser }) => {
     }
   };
 
-  const passRouteUpdate = (userData,userDetail) => {
+  const passRouteUpdate = (userData, userDetail) => {
     // Navigate to the skill-page route with userData
     setCheck(true);
-    navigate("/update-user-details", { state: { userData: userData,userDetail:userDetail } });
+    navigate("/update-user-details", {
+      state: { userData: userData, userDetail: userDetail },
+    });
   };
-
 
   return (
     <div>
@@ -152,13 +191,13 @@ const UserDetails = ({ setCheck, setCheckApprover, setCheckUser }) => {
                       <div className="col-md-6 ">
                         <div className="card ">
                           <div className="card-body ">
+                            <h3>Details</h3>
                             <p className="card-text">
                               <strong>Name:</strong> {userDetail.firstName}{" "}
                               {userDetail.lastName}
                             </p>
                             <p className="card-text">
                               <strong>Employee ID:</strong> {userDetail.emp_id}{" "}
-                              
                             </p>
                             <p className="card-text">
                               <strong>Email:</strong> {userDetail.email}
@@ -184,10 +223,12 @@ const UserDetails = ({ setCheck, setCheckApprover, setCheckUser }) => {
                               }
                             </p>
                             {/* Add more details as needed */}
-                            <button className="btn btn-primary"      onClick={() => {
-                      
-                      passRouteUpdate(userData,userDetail);
-                    }}>
+                            <button
+                              className="btn btn-primary"
+                              onClick={() => {
+                                passRouteUpdate(userData, userDetail);
+                              }}
+                            >
                               Update
                             </button>
                             {/* Add more details as needed */}
@@ -201,6 +242,93 @@ const UserDetails = ({ setCheck, setCheckApprover, setCheckUser }) => {
               </div>
             </div>
             <div className="mt-5"></div>
+            {/*skills*/}
+            <div className="card ">
+            <div className="card-body ">
+            <div >
+              <div className="row">
+                <h3>Skills</h3>
+                {skills.map((skill) => (
+                  <div className="col-md-4 mb-3" key={skill._id}>
+                    <div className="card">
+                      <div
+                        className="card-body"
+                        style={{
+                          maxHeight: "180px",
+                          overflow: "auto",
+                          scrollbarWidth: "thin",
+                        }}
+                      >
+                        <h5 className="card-title">{skill.name}</h5>
+                        <p className="card-text">
+                          Proficiency: {skill.proficiency}
+                        </p>
+                        <p className="card-text">
+                          Description: {skill.description}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            </div>
+              </div>
+            {/*certficate*/}
+            <div className="card mt-2 mb-2">
+            <div className="card-body ">
+            <div className="row">
+              {/* Display existing certificates */}
+              <h3>Certificates</h3>
+              {certificates.map((certificate, index) => (
+                <div className="col-md-4 mb-4" key={index}>
+                  <div
+                    className="card"
+                    style={{
+                      maxHeight: "300px",
+                      overflow: "auto",
+                      scrollbarWidth: "thin",
+                    }}
+                  >
+                    <div className="card-body">
+                      <h4 className="card-title">{certificate.title}</h4>
+                      <p className="card-text">
+                        <strong>Issuing Organization:</strong>{" "}
+                        {certificate.issuingOrganization}
+                      </p>
+                      <p className="card-text">
+                        <strong>Certificate ID:</strong>{" "}
+                        {certificate.certificate_id}
+                      </p>
+                      <p className="card-text">
+                        <strong>Issuance Date:</strong>{" "}
+                        {
+                          new Date(certificate.issuanceDate)
+                            .toISOString()
+                            .split("T")[0]
+                        }
+                      </p>
+                      <p className="card-text">
+                        <strong>Expiration Date:</strong>{" "}
+                        {
+                          new Date(certificate.expirationDate)
+                            .toISOString()
+                            .split("T")[0]
+                        }
+                      </p>
+                      <p className="card-text">
+                        <strong>Description:</strong> {certificate.description}
+                      </p>
+                      <p className="card-text">
+                        <strong>Status:</strong> {certificate.approval}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            </div>
+              </div>
           </div>
         </div>
       </div>
